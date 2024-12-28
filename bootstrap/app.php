9b8232e90370ef21c94716cfd44a5eeb313fdf8d
Usman Sher <uskhan099@gmail.com>
2024-12-28 16:35:45 +0200

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -24,6 +24,12 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'verified' => \App\Http\Middleware\EnsureEmailIsVerified::class,
         ]);
+        $middleware->validateCsrfTokens(except: [
+            'sanctum/csrf-cookie',
+            'api/*',
+            'login',
+
+        ]);
     })
     ->withSchedule(function (Schedule $schedule) {
         $schedule->command('app:backup-tenants')->weekly();

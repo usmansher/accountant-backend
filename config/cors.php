71c9baf69456815180ac70cfaf75286eba336a75
Usman Sher <uskhan099@gmail.com>
2024-12-28 16:19:37 +0200

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000', 'https://accountant-frontend-l1ivl03az-usmanshers-projects.vercel.app')],
+    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000'), 'https://accountant-frontend-l1ivl03az-usmanshers-projects.vercel.app'],
 
     'allowed_origins_patterns' => [],
 

7e71d24159531232bd3da3fda9d0a9172c6cf78f
Usman Sher <uskhan099@gmail.com>
2024-12-28 18:13:15 +0200

    Update Account.php

--- app/Models/Account.php
+++ app/Models/Account.php
@@ -6,12 +6,13 @@ use App\Traits\ActivityTrait;
 use Spatie\Activitylog\LogOptions;
 use Stancl\Tenancy\Database\Models\Tenant as BaseTenant;
 use Stancl\Tenancy\Contracts\TenantWithDatabase;
+use Stancl\Tenancy\Database\Concerns\CentralConnection;
 use Stancl\Tenancy\Database\Concerns\HasDatabase;
 use Stancl\Tenancy\Database\Concerns\HasDomains;
 
 class Account extends BaseTenant implements TenantWithDatabase
 {
-    use HasDatabase, HasDomains, ActivityTrait;
+    use HasDatabase, HasDomains, ActivityTrait, CentralConnection;
 
     public $incrementing = false;
     protected $keyType = 'string';
